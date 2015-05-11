8fda76efb692ae3984539867dc0aaefc950a4ff2
mario <mario.callisto@gmail.com>
2015-05-11 20:09:25 +0200

    Better style

--- src/main/scala-2.11/WebcamTest.scala
+++ src/main/scala-2.11/WebcamTest.scala
@@ -6,20 +6,20 @@ import com.github.sarxos.webcam.WebcamResolution
 
 object WebcamTest extends App {
 
-		val webcam = Webcam.getDefault()
-		webcam.setViewSize(WebcamResolution.VGA.getSize())
+  val webcam = Webcam.getDefault()
+  webcam.setViewSize(WebcamResolution.VGA.getSize())
 
-		val panel = new WebcamPanel(webcam)
-		panel.setFPSDisplayed(true)
-		panel.setDisplayDebugInfo(true)
-		panel.setImageSizeDisplayed(true)
-		panel.setMirrored(true)
+  val panel = new WebcamPanel(webcam)
+  panel.setFPSDisplayed(true)
+  panel.setDisplayDebugInfo(true)
+  panel.setImageSizeDisplayed(true)
+  panel.setMirrored(true)
 
-		val window = new JFrame("Test webcam panel")
-		window.add(panel)
-		window.setResizable(true)
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
-		window.pack()
-		window.setVisible(true)
+  val window = new JFrame("Test webcam panel")
+  window.add(panel)
+  window.setResizable(true)
+  window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+  window.pack()
+  window.setVisible(true)
 
 }
